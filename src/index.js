412b672e9a095ee441a8b56098ca88a0dcfc8ee9
rozh bahman tahir <r.bahman98@gmail.com>
2022-12-11 00:47:12 +0300

    Implement required changes to update list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import "./style.css";
-import getToDos, { addItem, deleteItem, updateTodo } from "./addDelete.js";
+import './style.css';
+import { checkoffItem } from './interactiveList.js';
+import getToDos, { addItem, deleteItem, updateTodo } from './addDelete.js';
 
-const todoContainer = document.querySelector(".todo-list");
+const todoContainer = document.querySelector('.todo-list');
+const clearListBtn = document.querySelector('.btn-clear');
 
 const generateMarkup = (data) => {
   return `<li class="todo" data-index="${data.index}" data-completed="${data.completed}"> 
-  <label for="${data.index}"> <input type="checkbox"> </label>
+  <label for="${data.index}"> <input type="checkbox" class="checkbox"></label>
   <input type="text" id="${data.index}" class="item-description-input" name="${data.index}" value="${data.description}">
    <div class="fa-list-icon"><i
   class="fa-solid fa-ellipsis-vertical"></i></div> 
@@ -13,7 +15,7 @@ const generateMarkup = (data) => {
 };
 
 const createList = (arr) => {
-  let listString = "";
+  let listString = '';
 
   const sortedArr = arr.sort((a, b) => a.index - b.index);
 
@@ -25,22 +27,22 @@ const createList = (arr) => {
 };
 
 // Add to do
-window.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
     const { value } = e.target;
 
     if (!value?.trim()) return;
 
-    if (e.target.classList.contains("add-todo-input")) {
+    if (e.target.classList.contains('add-todo-input')) {
       addItem(value);
       // Empty input
-      e.target.value = "";
+      e.target.value = '';
       createList(getToDos());
       return;
     }
 
-    if (e.target.classList.contains("item-description-input")) {
-      const parentEl = e.target.closest(".todo");
+    if (e.target.classList.contains('item-description-input')) {
+      const parentEl = e.target.closest('.todo');
 
       const { index } = parentEl.dataset;
 
@@ -51,24 +53,41 @@ window.addEventListener("keydown", (e) => {
 });
 
 // Deleting
-todoContainer.addEventListener("click", (e) => {
-  const itemSelected = e.target.closest(".fa-list-icon");
+todoContainer.addEventListener('click', (e) => {
+  const itemSelected = e.target.closest('.fa-list-icon');
 
   if (!itemSelected) return;
 
   itemSelected
-    .querySelector(".fa-solid")
-    .classList.remove("fa-ellipsis-vertical");
+    .querySelector('.fa-solid')
+    .classList.remove('fa-ellipsis-vertical');
 
-  itemSelected.querySelector(".fa-solid").classList.add("fa-trash-can");
+  itemSelected.querySelector('.fa-solid').classList.add('fa-trash-can');
 
-  const trashIcon = itemSelected.querySelector(".fa-trash-can");
+  const trashIcon = itemSelected.querySelector('.fa-trash-can');
 
-  trashIcon?.addEventListener("click", () => {
-    const parentEle = trashIcon.closest(".todo");
+  trashIcon?.addEventListener('click', () => {
+    const parentEle = trashIcon.closest('.todo');
     const { index } = parentEle.dataset;
 
     deleteItem(+index);
     createList(getToDos());
   });
+});
+
+let indexesToRemove = [];
+
+todoContainer.addEventListener('click', (e) => {
+  const activeEl = e.target.closest('.checkbox');
+  if (!activeEl) return;
+  const parentEl = activeEl.closest('.todo');
+
+  indexesToRemove = checkoffItem(parentEl);
+});
+
+clearListBtn.addEventListener('click', () => {
+  indexesToRemove.forEach((i) => {
+    deleteItem(i);
+  });
+  createList(getToDos());
 });
